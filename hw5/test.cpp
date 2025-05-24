8ac93e5c3e04d0947e5b2cc1a08bdbbf1c456f67
palina-mrk <khudziakova@tut.by>
2025-05-24 17:07:50 +0300

    hw5

--- hw5/test.cpp
+++ hw5/test.cpp
@@ -7,15 +7,17 @@
 class IStatistics {
 public:
 	virtual ~IStatistics() {}
-
+  IStatistics() : N{0} {}
 	virtual void update(double next) = 0;
 	virtual double eval() const = 0;
 	virtual const char * name() const = 0;
+protected:
+  std::size_t N;
 };
 
 class Min : public IStatistics {
 public:
-	Min() : m_min{std::numeric_limits<double>::max()}, N{0} {
+	Min() : m_min{std::numeric_limits<double>::max()} {
 	}
 
 	void update(double next) override {
@@ -35,12 +37,11 @@ public:
 
 private:
 	double m_min;
-  int N;
 };
 
 class Max : public IStatistics {
 public:
-  Max() : m_max{std::numeric_limits<double>::min()}, N{0} {
+  Max() : m_max{std::numeric_limits<double>::min()} {
   }
 
   void update(double next) override {
@@ -61,12 +62,11 @@ public:
 private:
   // m_max = DOUBLE_MAX
   double m_max;
-  size_t N;
 };
 
 class Mean : public IStatistics {
 public:
-  Mean() : m_sum{0}, N{0} {
+  Mean() : m_sum{0} {
   }
 
   void update(double next) override {
@@ -84,12 +84,11 @@ public:
 
 private:
   double m_sum;    // сумма
-  size_t N;        // длина массива
 };
 
 class StandardDeviation : public IStatistics {
 public:
-  StandardDeviation() : m_sum_sqr{0}, m_sum{0}, N{0} {
+  StandardDeviation() : m_sum_sqr{0}, m_sum{0} {
   }
 
   void update(double next) override {
@@ -101,9 +100,9 @@ public:
   double eval() const override {
     if(!N)
       return 0;
-    double squared_result, m_mean;
-    m_mean = m_sum / (double) N;
-    squared_result = m_sum_sqr/(double) N - m_mean*m_mean;
+    double squared_result, mean;
+    mean = m_sum / (double) N;
+    squared_result = m_sum_sqr/(double) N - mean*mean;
     return std::sqrt(squared_result);
   }
 
@@ -114,16 +113,15 @@ public:
 private:
   double m_sum_sqr; // сумма квадратов
   double m_sum;    // сумма
-  size_t N;        // длина массива
 };
 
 
 int main() {
-    std::vector<std::unique_ptr<IStatistics>> stats;
-    stats.emplace_back(dynamic_cast<IStatistics*>(new Min));
-    stats.emplace_back(dynamic_cast<IStatistics*>(new Max));
-    stats.emplace_back(dynamic_cast<IStatistics*>(new Mean));
-    stats.emplace_back(dynamic_cast<IStatistics*>(new StandardDeviation));
+    std::vector<IStatistics*> stats;
+    stats.emplace_back(new Min);
+    stats.emplace_back(new Max);
+    stats.emplace_back(new Mean);
+    stats.emplace_back(new StandardDeviation);
 
     double val;
     while (std::cin >> val) {
@@ -133,5 +131,6 @@ int main() {
     }
     for (auto& stat: stats) {
         std::cout << stat->name() << ":\t" << stat->eval() << std::endl;
+        delete stat;
     }
 }
